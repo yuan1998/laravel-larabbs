b42bf87f5120f2fba2fe09183ae248966f2bd15b
yuan1998 <32232389+yuan1998@users.noreply.github.com>
2018-05-13 18:00:36 +0800

    create layouts, pages root,pagesController

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/helpers.php';
+require_once __DIR__ . '/helper.php';
 /*
 |--------------------------------------------------------------------------
 | Create The Application
