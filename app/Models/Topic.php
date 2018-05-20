ef52d0000301a1dcb71db8df6fe99f3b6a105e29
yuan1998 <chizhiyueshu@gmail.com>
2018-05-20 09:02:08 +0000

    Topics belongsTo User & Category

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -7,4 +7,21 @@ class Topic extends Model
     protected $fillable = [
         'title', 'body', 'user_id', 'category_id', 'reply_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'
     ];
+
+
+
+    public function category ()
+    {
+        return $this->belongsto(Category::class);
+    }
+
+
+
+    public function user ()
+    {
+        return $this->belongsto(User::class);
+    }
+
+
+
 }
