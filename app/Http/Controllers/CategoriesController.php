b95883369ce3898a4ab15a89355afbabda6b0d08
yuan1998 <chizhiyueshu@gmail.com>
2018-05-20 09:58:19 +0000

    Add category topic list

---
+++ app/Http/Controllers/CategoriesController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Topic;
+use App\Models\Category;
+
+class CategoriesController extends Controller
+{
+
+
+    public function show (Category $category)
+    {
+        $topics = Topic::where('category_id', $category->id)->paginate(20);
+
+        // 传参变量话题和分类到模板中
+        return view('topics.index', compact('topics', 'category'));
+
+    }
+}
